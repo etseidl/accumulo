e5b273afc7f6abd01bd645910b02dc62349d54c4
Ed Seidl <seidl2@llnl.gov>
2019-12-09 12:51:23 -0800

    change property validation to match what server does... wasn't handling custom properties correctly

--- core/src/test/java/org/apache/accumulo/core/clientImpl/NamespaceOperationsImplTest.java
+++ core/src/test/java/org/apache/accumulo/core/clientImpl/NamespaceOperationsImplTest.java
@@ -27,6 +27,7 @@ import org.apache.accumulo.core.client.NamespaceNotFoundException;
 import org.apache.accumulo.core.conf.Property;
 import org.apache.accumulo.core.conf.PropertyType;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.core.classloader.annotations.PrepareForTest;
--- core/src/test/java/org/apache/accumulo/core/clientImpl/TableOperationsImplTest.java
+++ core/src/test/java/org/apache/accumulo/core/clientImpl/TableOperationsImplTest.java
@@ -26,6 +26,7 @@ import org.apache.accumulo.core.client.AccumuloSecurityException;
 import org.apache.accumulo.core.conf.Property;
 import org.apache.accumulo.core.conf.PropertyType;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.core.classloader.annotations.PrepareForTest;
