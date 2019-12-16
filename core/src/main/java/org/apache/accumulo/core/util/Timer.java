8d5e64280c08d99f3806a6b9660715b52a539de2
Ed Seidl <seidl2@llnl.gov>
2019-12-16 14:29:18 -0800

    add timer code

---
+++ core/src/main/java/org/apache/accumulo/core/util/Timer.java
@@ -0,0 +1,127 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.accumulo.core.util;
+
+public class Timer {
+  // total cpu (user + system). gets both in one call to getrusage()
+  public static native double getProcessCpuTime();
+
+  // just user
+  public static native double getProcessUserTime();
+
+  // just system
+  public static native double getProcessSystemTime();
+
+  // returns rusage struct as array. access via RUsage class
+  private static native void getProcessRUsage(long[] struct);
+
+  // total cpu (user + system). gets both in one call to getrusage()
+  public static native double getThreadCpuTime();
+
+  // just user
+  public static native double getThreadUserTime();
+
+  // just system
+  public static native double getThreadSystemTime();
+
+  // returns rusage struct as array. access via RUsage class
+  private static native void getThreadRUsage(long[] struct);
+
+  // TODO expose all members of struct rusage
+  public static class RUsage {
+    public enum RUsageType {
+      SELF, THREAD
+    };
+
+    private static final int POS_USER_SEC = 0;
+    private static final int POS_USER_USEC = 1;
+    private static final int POS_SYS_SEC = 2;
+    private static final int POS_SYS_USEC = 3;
+    private static final int POS_MAX_RSS = 4;
+    private static final int POS_IX_RSS = 5;
+    private static final int POS_ID_RSS = 6;
+    private static final int POS_IS_RSS = 7;
+    private static final int POS_MIN_FLT = 8;
+    private static final int POS_MAJ_FLT = 9;
+    private static final int POS_NSWAP = 10;
+    private static final int POS_IN_BLK = 11;
+    private static final int POS_OUT_BLK = 12;
+    private static final int POS_MSG_SND = 13;
+    private static final int POS_MSG_RCV = 14;
+    private static final int POS_N_SIG = 15;
+    private static final int POS_NVCSW = 16;
+    private static final int POS_NIVCSW = 17;
+
+    private static final int LEN_RUSAGE = POS_NIVCSW + 1;
+
+    private final long[] rusageArr = new long[LEN_RUSAGE];
+    private final RUsageType rutype;
+
+    // create struct and call getrusage() with default type of RUsageType.SELF
+    // this will return rusage for the process.
+    public RUsage() {
+      this(RUsageType.SELF);
+    }
+
+    // create struct and call getrusage() with passed in type. RUsageType.SELF
+    // will return rusage for the process, RUsageType.THREAD will return rusage
+    // for the current thread.
+    public RUsage(RUsageType type) {
+      rutype = type;
+      getRUsage();
+    }
+
+    // call getrusage() again...will overwrite old values. uses RUsageType passed
+    // in constructor
+    public void getRUsage() {
+      switch (rutype) {
+        case SELF:
+          Timer.getProcessRUsage(rusageArr);
+          break;
+        case THREAD:
+          Timer.getThreadRUsage(rusageArr);
+          break;
+      }
+    }
+
+    private double toMicros(long t, long ut) {
+      return (double) t + (double) ut * 0.000001;
+    }
+
+    public double userTime() {
+      return toMicros(rusageArr[POS_USER_SEC], rusageArr[POS_USER_USEC]);
+    }
+
+    public double systemTime() {
+      return toMicros(rusageArr[POS_SYS_SEC], rusageArr[POS_SYS_USEC]);
+    }
+
+    public double cpuTime() {
+      return userTime() + systemTime();
+    }
+
+    public long inBlock() {
+      return rusageArr[POS_IN_BLK];
+    }
+
+    public long outBlock() {
+      return rusageArr[POS_OUT_BLK];
+    }
+  }
+}
---
+++ core/src/main/java/org/apache/accumulo/core/util/TimerManager.java
@@ -0,0 +1,82 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.accumulo.core.util;
+
+import java.io.File;
+import java.util.HashMap;
+
+// singleton to manage per-thread RegionTimer instances.  kind of
+// annoying that I have to use HashMap (modernizer complained about
+// Hashtable, which is synchronized) rather than ConcurrentHashMap
+// due to spot-bugs flagging the non-atomic check/put.  Should figure
+// out how to suppress, since only the current thread can supply
+// the missing key...don't have to worry about another thread adding
+// this thread to the map.  Oh well, I don't expect this to be
+// called all that often so it shouldn't be a big issue...just be
+// sure to call once and save a timer, rather than repeatedly
+// calling timerForThread() inside loops, say.
+public class TimerManager {
+  private static final HashMap<Thread,RegionTimer> sThreadTimers = new HashMap<>();
+
+  static {
+    String envAccumuloHome = System.getenv("ACCUMULO_HOME");
+    File nativeDir = new File(envAccumuloHome + "/lib/native");
+    String libname = System.mapLibraryName("accumulo");
+    File libFile = new File(nativeDir, libname);
+    System.out.println("load " + libFile.getAbsolutePath());
+    try {
+      System.load(libFile.getAbsolutePath());
+    } catch (Exception e) {
+      e.printStackTrace();
+      throw e;
+    }
+  }
+
+  public static RegionTimer timerForThread() {
+    Thread curr = Thread.currentThread();
+
+    synchronized (sThreadTimers) {
+      if (!sThreadTimers.containsKey(curr)) {
+        RegionTimer timer = new RegionTimer(curr.getName());
+        sThreadTimers.put(curr, timer);
+        return timer;
+      }
+    }
+    return sThreadTimers.get(curr);
+  }
+
+  public static void setTimerForThread(RegionTimer timer) {
+    Thread curr = Thread.currentThread();
+    synchronized (sThreadTimers) {
+      // will remove existing timer for this thread if one
+      // exists
+      sThreadTimers.put(curr, timer);
+    }
+  }
+
+  public static RegionTimer removeTimerForThread() {
+    Thread curr = Thread.currentThread();
+    synchronized (sThreadTimers) {
+      if (sThreadTimers.containsKey(curr)) {
+        return sThreadTimers.remove(curr);
+      }
+    }
+    return null;
+  }
+}
